b888f035cfe9a174d67f48bfcba098d91decee84
TravisPRyan <43801485+TravisPRyan@users.noreply.github.com>
2019-01-29 15:08:03 -0500

    Add Note functionality
    added: 
    add note feature
    
    refactored:
    click listeners to account for dynamically added elements

--- toDoList/assets/js/scripts.js
+++ toDoList/assets/js/scripts.js
@@ -1,15 +1,28 @@
 // click to check off
 //toggles between done class and null display
-$("li").on("click", function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("done", "null");
 });
 
 //click on # to delete 
 //layer event listeners to prevent event bubbling
 //remove hidden elements for improved performance
-$("span").on("click", function(e){
+$("ul").on("click", "span", function(e){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
 	e.stopPropagation();
 });
+
+//listens for enter keypress in text input 
+//applies input to var
+//clear input
+//append var to ul as li
+$('input[type="text"]').keypress(function(e){
+	if(e.which === 13){
+		var userInp = $(this).val();
+		$(this).val("");
+		$('ul').append("<li><span># </span>" + userInp + "</li>");
+	}
+});
+
